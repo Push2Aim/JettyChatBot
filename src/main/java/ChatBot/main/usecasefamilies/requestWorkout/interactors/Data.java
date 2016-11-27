8c151583fc5e3935ebe35cb68af0876179fbb427
Gott50 <xxGott50xx@gmail.com>
2016-11-27 14:21:16 +0100

    try 200 char message via Facebook without displayText

--- src/main/java/ChatBot/main/usecasefamilies/requestWorkout/interactors/Data.java
+++ src/main/java/ChatBot/main/usecasefamilies/requestWorkout/interactors/Data.java
@@ -4,7 +4,7 @@ class Data {
     FaceBook facebook = new FaceBook();
 
     Data(String message) {
-        facebook.text = message.substring(0, 100);
+        facebook.text = message.substring(0, 200);
     }
 
     private class FaceBook {
--- src/main/java/ChatBot/main/usecasefamilies/requestWorkout/interactors/RequestWorkout_Response.java
+++ src/main/java/ChatBot/main/usecasefamilies/requestWorkout/interactors/RequestWorkout_Response.java
@@ -20,7 +20,7 @@ public class RequestWorkout_Response {
     String displayText;
 
     public RequestWorkout_Response(String speech) {
-        displayText = speech;
+        //displayText = speech;
         this.speech = speech;
         this.data = new Data(speech);
     }
